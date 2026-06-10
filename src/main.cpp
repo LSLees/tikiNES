8d0d5fdceaa018756e6fcddc857bd5bd1adb8d6c
TintinF1754 <160863259+TintinF1754@users.noreply.github.com>
2026-06-10 21:29:33 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,24 @@ int main(int argc, char* argv[])
 	WRAM* wram = new WRAM;
 	cpu->displayReg();
 
+	// Open .nes file
+	std::ifstream file("../test/Super Mario Bros. (World).nes", std::ios::binary);
+
+	if (!file)
+	{
+		std::cout << "Failed\n";
+		return 1;
+	} 
+
+	// Reading first 8 bytes
+	unsigned char bytes[8];
+	file.read(reinterpret_cast<char*>(bytes), 8);
+
+	// Print first 8 bytes
+	for (int i = 0; i < 8; i++)
+	{
+		std::cout << std::hex << (int)bytes[i] << ' ';
+	}
+
 	return 0;
 }
