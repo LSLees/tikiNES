b64a0423a7cf72e4addc689665b18d3e3212db95
TintinF1754 <160863259+TintinF1754@users.noreply.github.com>
2026-06-17 21:05:31 +0100

    CMake installed properly
    Added contrib and cleaned the project

--- src/main.cpp
+++ src/main.cpp
@@ -21,16 +21,12 @@ int main(int argc, char* argv[])
 	NES* nes = new NES;
 	nes->cpu.Step();
 	delete nes;
-	return 0;
+	//return 0;
 
 	// Open .nes file
-	std::ifstream file("test/Super Mario Bros. (World).nes", std::ios::binary);
+	std::ifstream file("../test/Super Mario Bros. (World).nes", std::ios::binary);
 
-	if (!file)
-	{
-		std::cout << "Failed\n";
-		return 1;
-	}
+	
 
 	// Reading first 8 bytes
 	unsigned char bytes[8];
@@ -40,6 +36,17 @@ int main(int argc, char* argv[])
 	for (int i = 0; i < 8; i++)
 	{
 		std::cout << std::hex << (int)bytes[i] << ' ';
+	}
+
+	if (!file)
+	{
+		std::cout << "Failed\n";
+		return 1;
+	}
+	else
+	{
+		std::cout << "Found\n";
+		return 0;
 	}
 
 	delete nes;
