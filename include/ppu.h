61bce44b54919c61718f9e81f0701c443fcf1e9b
TintinF1754 <160863259+TintinF1754@users.noreply.github.com>
2026-07-27 15:39:56 +0100

    Updated some PPU

--- include/ppu.h
+++ include/ppu.h
@@ -1,9 +1,36 @@
 #pragma once
 
+#include <array>
+
 struct PPU
 {
 	void Step();
 
+	// Reset PPU state
+	void Reset();
+
+	// One cycle
+	void Clock();
+
+	U8 Status = 0;
+
 private:
+
+	// Connect cartridge
+	Cartridge* m_cartridge = nullptr;
+
+	// Internal VRAM (2KB)
+	std::array<U8, 2048> m_nameTables{};
+
+
+
+
+	// Timing
+	int m_cycle = 0;
+	int m_scanline = 0;
+
+	bool m_frameCompleted = false;
+
+
 
 };
